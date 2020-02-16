4214cde86844da6a389f53a99ed96a7cbcc9c7d2
manulangat <emmanuelthedeveloper@gmail.com>
2020-02-16 21:15:48 +0300

    updating the readme.md file

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('',PicturesAPI.as_view(),name="pictures"),
     path('feed/',PictureAPI.as_view(),name="picture"),
     path('comments/',CommentAPI.as_view(),name="comments"),
-    path('likes/',LikesAPI.as_view(),name="likes"),
+    path('likes/<pk>/',LikesAPI.as_view(),name="likes"),
 ]
